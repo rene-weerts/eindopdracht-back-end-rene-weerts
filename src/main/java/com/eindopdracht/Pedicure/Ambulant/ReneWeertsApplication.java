106d4b479d24087a52cbe757ff3a79b8fc5d7fd4
“rene-weerts” <rjmwrt@gmail.com>
2023-05-10 19:42:39 +0200

    initial-commit

---
+++ src/main/java/com/eindopdracht/Pedicure/Ambulant/ReneWeertsApplication.java
@@ -0,0 +1,13 @@
+package com.eindopdracht.Pedicure.Ambulant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReneWeertsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReneWeertsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eindopdracht/Pedicure/Ambulant/ReneWeertsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eindopdracht.Pedicure.Ambulant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReneWeertsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
